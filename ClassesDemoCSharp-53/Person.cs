cdb87170962eba44bfc616db49af2a317c118408
Seth Bowman <brandonsethbowman@gmail.com>
2024-12-31 14:42:59 -0600

    Demo completed

---
+++ ClassesDemoCSharp-53/Person.cs
@@ -0,0 +1,35 @@
+﻿namespace ClassesDemoCSharp_53;
+
+public class Person
+{
+    //fields - a variable inside of a class (camelCase)
+    public double _wallet = 0;
+    public string _clothes;
+    
+    //properties - things that define the class (PascalCase)
+    public string Name { get; set; }
+    public int Age { get; set; } = 0;
+    
+    //methods (non static methods for now)
+    public void Greet()
+    {
+        Console.WriteLine($"Hello, my name is {Name}!");
+    }
+    
+    //constructors
+    
+    //custom constructor
+    public Person(double wallet, string clothes, string name, int age)
+    {
+        _wallet = wallet;
+        _clothes = clothes;
+        Name = name;
+        Age = age;
+    }
+    
+    //default constructor
+    public Person()
+    {
+        
+    }
+}
---
+++ ClassesDemoCSharp-53/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ClassesDemoCSharp_53;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //dot notation
+        Person seth = new Person();
+        seth._clothes = "t shirt and slacks";
+        seth._wallet = 200.00;
+        seth.Name = "Seth";
+        seth.Age = 30;
+        
+        seth.Greet();
+        
+        //object initializer syntax
+        var bill = new Person()
+        {
+            Name = "Bill",
+            Age = 24,
+            _clothes = "suit",
+            _wallet = 300.00
+        };
+        
+        bill.Greet();
+        
+        //using a custom constructor
+        var bob = new Person(500.00, "blue suit", "Bob", 40);
+    }
+}
